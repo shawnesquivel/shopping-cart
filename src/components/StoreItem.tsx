e1b00633872d4d2897a41ee3dc756a4586a55a0e
shawnesquivel <shawnesquivel24@gmail.com>
2022-11-23 13:59:21 -0800

    formatted cards

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,32 @@
+import { Card } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
+
+type StoreItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
+
+export const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
+  const quantity = 0;
+
+  return (
+    <Card className="h-100">
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200px"
+        style={{ objectFit: "cover" }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+      </Card.Body>
+
+      <div className="mt-auto"></div>
+    </Card>
+  );
+};
