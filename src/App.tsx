605c1dab8b8e337c0122c14fef8a9663bcd85ef7
shawnesquivel <shawnesquivel24@gmail.com>
2022-11-23 10:51:45 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <h1>Hi</h1>
+      <h2>yo</h2>
+      <p>This is a shopping cart website</p>
+    </>
+  );
 }
 
 export default App;
